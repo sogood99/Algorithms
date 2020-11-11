d4b2238da1e560b1c41c725eb7cc7e5d5f67e75e
H <>
2020-11-11 15:42:31 +0800

    Change dir structure

---
+++ Sorting/bubblesort.hpp
@@ -0,0 +1,9 @@
+#ifndef BUBBLESORT_HPP
+#define BUBBLESORT_HPP
+
+template <typename T>
+void bubbleSort(T* array){
+
+}
+
+#endif // BUBBLESORT_HPP
---
+++ Sorting/mergesort.hpp
@@ -0,0 +1,9 @@
+#ifndef MERGESORT_HPP
+#define MERGESORT_HPP
+
+template <typename T>
+void mergeSort(T* array, int l, int r){
+
+}
+
+#endif // MERGESORT_HPP
