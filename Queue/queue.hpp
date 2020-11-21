cc49a28f95c8e9f5c5fbbb18e7fd6f1751527b53
H <>
2020-11-21 16:35:01 +0800

    Added queue

---
+++ Queue/queue.hpp
@@ -0,0 +1,46 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+#define MAX_QUEUESIZE 10000
+
+template <typename T>
+class queue{
+public:
+    queue(){}
+public:
+    bool enque(T item);
+    T dequeue();
+    bool empty();
+protected:
+    T m_Queue[MAX_QUEUESIZE + 1];
+    int m_Head = 0;
+    int m_Tail = 0;
+};
+
+template <typename T>
+bool queue<T>::enque(T item){
+    if ((m_Tail + 1)%(MAX_QUEUESIZE+1) == m_Head){
+        return false;
+    }
+    m_Queue[m_Tail] = item;
+    m_Tail++;
+    m_Tail %= (MAX_QUEUESIZE+1);
+    return true;
+}
+
+template <typename T>
+T queue<T>::dequeue(){
+    int last = m_Head;
+    if (m_Head == m_Tail){
+        return T();
+    }
+    m_Head ++;
+    m_Head %= (MAX_QUEUESIZE+1);
+    return m_Queue[last];
+}
+
+template <typename T>
+bool queue<T>::empty(){
+    return m_Head == m_Tail;
+}
+
+#endif // QUEUE_HPP
